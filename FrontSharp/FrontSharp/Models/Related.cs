63aa073a2b562c1d15619953f993935a135b0f86
Callen Barton <callen.barton@fishbowlinventory.com>
2017-06-05 17:39:13 -0600

    Added additional fields for related modules.

--- FrontSharp/FrontSharp/Models/Related.cs
+++ FrontSharp/FrontSharp/Models/Related.cs
@@ -8,10 +8,16 @@ namespace FrontSharp.Models
 {
     public class Related
     {
+        public string channels { get; set; }
+        public string conversation { get; set; }
+        public string conversations { get; set; }
+        public string comments { get; set; }
+        public string contact { get; set; }
         public string events { get; set; }
         public string followers { get; set; }
-        public string messages { get; set; }
-        public string comments { get; set; }
         public string inboxes { get; set; }
+        public string message_replied_to { get; set; }
+        public string messages { get; set; }
+        public string teammates { get; set; }
     }
 }
