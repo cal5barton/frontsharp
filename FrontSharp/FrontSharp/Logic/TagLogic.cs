a57b298d83ae238169fcec2dd80bf949b3ce53c2
Callen Barton <callen.barton@fishbowlinventory.com>
2017-06-15 11:46:30 -0600

    Implemented Create Tag Logic.

---
+++ FrontSharp/FrontSharp/Logic/TagLogic.cs
@@ -0,0 +1,34 @@
+﻿using FrontSharp.Interfaces;
+using FrontSharp.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontSharp.Logic
+{
+    public class TagLogic : BaseLogic, ITagLogic
+    {
+        public TagLogic(FrontSharpClient client)
+            : base(client)
+        {
+            _baseRoute = "tags";
+        }
+
+        public Tag CreateTag(string name)
+        {
+            var request = new RestRequest();
+            request.Resource = _baseRoute;
+            request.Method = Method.POST;
+
+            //Create anonymous object with data. API returning failure response unless this is anonymous for whatever reason
+            var obj = new { name = name};
+
+            var response = _client.Execute<Tag>(request, obj);
+
+            return response;
+        }
+    }
+}
