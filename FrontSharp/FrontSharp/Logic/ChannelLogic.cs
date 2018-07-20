d0b928e6881bcb8e2bbc275f1b64be9e3362ad27
Anthony Frank <anthony.frank@fishbowlinventory.com>
2018-07-19 23:18:12 -0600

    Added get channel functionality

---
+++ FrontSharp/FrontSharp/Logic/ChannelLogic.cs
@@ -0,0 +1,28 @@
+﻿using FrontSharp.Interfaces;
+using FrontSharp.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontSharp.Logic
+{
+    public class ChannelLogic : BaseLogic, IChannelLogic
+    {
+        public ChannelLogic(FrontSharpClient client) : base(client)
+        {
+            _baseRoute = "channels";
+        }
+
+        public Channel Get(string channelAddress)
+        {
+            var request = base.BuildRequest();
+            request.Resource += "/alt:address:{channelAddress}";
+            request.AddParameter("channelAddress", channelAddress, ParameterType.UrlSegment);
+
+            return _client.Execute<Channel>(request);
+        }
+    }
+}
---
+++ FrontSharp/FrontSharp/Models/Channel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontSharp.Models
+{
+    public class Channel
+    {
+        public string id { get; set; }
+        public string address { get; set; }
+        public string type { get; set; }
+        public string send_as { get; set; }
+        public bool is_private { get; set; }
+    }
+}
